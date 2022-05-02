0b04ca2f875521d784668988c77c78b805779c5c
Jimmy <jimmysana@gmail.com>
2022-05-02 22:49:25 +0100

    Added ServiceResponseMessage class

---
+++ Product.Api/Constants/ServiceResponseMessages.cs
@@ -0,0 +1,9 @@
+﻿namespace Product.Api.Constants
+{
+    public class ServiceResponseMessages
+    {
+        public const string InvalidGuid = "The guid supplied was invalid";
+        public const string ProductDoesNotExist = "The requested product does not exist";
+        public const string UnexpectedError = "There was an unexpedted error.";
+    }
+}
