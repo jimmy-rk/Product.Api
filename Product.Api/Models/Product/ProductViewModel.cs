c87387027f77c82f19609103193acff4ff76944d
Jimmy <jimmysana@gmail.com>
2022-05-02 22:44:43 +0100

    Added product view model

---
+++ Product.Api/Models/Product/ProductViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace Product.Api.Models.Product
+{
+    public class ProductViewModel
+    {
+        [JsonProperty("guid")]
+        public string Guid { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("productGroupNk")]
+        public string ProductGroupNk { get; set; }
+
+        [JsonProperty("price")]
+        public decimal Price { get; set; }
+
+        [JsonProperty("comments")]
+        public string Comments { get; set; }
+    }
+}
