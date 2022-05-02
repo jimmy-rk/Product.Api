a251ce05abe3f6097178785b7b4605a38ae5acea
Jimmy <jimmysana@gmail.com>
2022-05-02 22:28:47 +0100

    Added product controller

---
+++ Product.Api/Controller/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Product.Api.Services.Product;
+
+namespace Product.Api.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<ProductController> _logger;
+        private readonly IProductService _productService;
+
+        public ProductController(ILogger<ProductController> logger, IProductService productService)
+        {
+            _logger = logger;
+            _productService = productService;
+        }
+    }
+}
