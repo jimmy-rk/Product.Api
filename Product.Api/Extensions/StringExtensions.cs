cb0900560c4aa6aa7cf7573a1e5de68e6e5415eb
Jimmy <jimmysana@gmail.com>
2022-05-02 23:07:43 +0100

    added cqrs update class

---
+++ Product.Api/Extensions/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Product.Api.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsNullOrEmptyOrInvalidGuid(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return true;
+            return !Guid.TryParse(input, out Guid guid);    
+        }
+    }
+}
