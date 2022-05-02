3142fcc56bdf901892e278889c6fe604c4ab0701
Jimmy <jimmysana@gmail.com>
2022-05-02 22:18:42 +0100

    Added mapping class

---
+++ Product.Api/Services/MappingProfiles/ProductMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Product.Api.Models.General;
+
+namespace Product.Api.Services.MappingProfiles
+{
+    public class ProductMappingProfile : Profile
+    {
+        public ProductMappingProfile()
+        {
+            CreateMap<Cqrs.Product.GetProductGroupTypes.Response, SelectListItem>(MemberList.None)
+            .ForPath(dest => dest.Value, opt => opt.MapFrom(src => src.Value))
+            .ForPath(dest => dest.Text, opt => opt.MapFrom(src => src.Text));
+        }
+    }
+}
